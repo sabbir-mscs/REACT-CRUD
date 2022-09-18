8dd0713d351d5b797abe4be367adb76fea91c4fd
SABBIR AHMED <sabbir.mscs@gmail.com>
2022-09-18 18:43:29 +0600

    StudentList Component Added

---
+++ react-mernstack-crud/src/components/edit-student.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class EditStudent extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Edit Student Component!</p>
+      </div>
+    );
+  }
+}
---
+++ react-mernstack-crud/src/components/student-list.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class StudentList extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Student List Component!</p>
+      </div>
+    );
+  }
+}
