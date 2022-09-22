38da0ab4cd3aa979e59b839dda317ea8dc06dae9
SABBIR AHMED <sabbir.mscs@gmail.com>
2022-09-22 15:09:20 +0600

    Individual ToDo can be added

--- basic-crud-1/src/App.js
+++ basic-crud-1/src/App.js
@@ -5,13 +5,19 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>TODO LIST</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} />
     </div>
   );
 }
